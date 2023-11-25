c286f814ca96411e4f46d94d41dc41f121e77122
mysd <136174825+YamanSD@users.noreply.github.com>
2023-11-25 17:58:04 +0200

    Delete server/build directory

--- server/build/model/SessionAttendee.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const database_1 = require("../database");
-/*
- * Simple junction table for the User-Session M-N attendee association.
- * Foreign keys from each table are generated
- */
-const SessionAttendee = database_1.database.define('Attendee', {});
-exports.default = SessionAttendee;
